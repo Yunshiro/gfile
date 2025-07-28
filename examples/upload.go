cb6e656eb2376d87f34446d7d30b2d23848e2769
Yunshiro <cookiemakefun@qq.com>
2025-07-28 20:00:03 +0800

    file to bytes

--- examples/upload.go
+++ examples/upload.go
@@ -5,5 +5,7 @@ import (
 )
 
 func main() {
-	gfile.Upload()
+	// wd, _ := os.Getwd()
+	// fmt.Println("当前工作目录:", wd) // 检查程序运行位置
+	gfile.Upload("examples/gopher.png")
 }
--- upload.go
+++ upload.go
@@ -2,6 +2,8 @@ package gfile
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 // MultipartForm represents one part of file parts in HTTP request.
@@ -12,7 +14,22 @@ type MultipartForm struct {
 	ContentDisposition 	string
 }
 
-func Upload() {
+func Transfer(filename string) []byte {
+	fileBytes := toBytes(filename)
+	return fileBytes
+}
+
+func toBytes(filename string) []byte {
+	bts, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("file to bytes error:%v", err)
+	}
+	return bts
+}
+
+
+
+func Upload(filename string) {
 	fmt.Println("upload success")
-	
+	fmt.Printf("bytes: %b\n", Transfer(filename))
 }
